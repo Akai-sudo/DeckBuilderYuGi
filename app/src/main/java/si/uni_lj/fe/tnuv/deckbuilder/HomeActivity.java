4dbb7f53aca010f4d2cfdcb12ae949baff96cb0d
AnaMauric <am7479@student.uni-lj.si>
2022-06-17 14:59:50 +0200

    poenotenje izgleda, ozadje, font...

--- app/src/main/java/si/uni_lj/fe/tnuv/deckbuilder/HomeActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/deckbuilder/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import si.uni_lj.fe.tnuv.deckbuilder.databinding.ActivityHomeBinding;
 import si.uni_lj.fe.tnuv.deckbuilder.deck.DeckActivity;
@@ -21,6 +22,15 @@ public class HomeActivity extends AppCompatActivity {
     public void buddyActivity(View v) {
         Intent intent = new Intent(HomeActivity.this, BuddyActivity.class);
         startActivity(intent);
+    }
+
+    public void displayToastMsg(View v) {
+        toastMsg("A new friend has been added!");
+    }
+
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
     }
 
 
