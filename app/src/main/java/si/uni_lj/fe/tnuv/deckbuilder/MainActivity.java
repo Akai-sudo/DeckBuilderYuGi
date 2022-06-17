fdbdef2141f9d00e8dbb609778fb28e6cc244dc0
Akai-sudo <mohorluka@gmail.com>
2022-06-17 20:55:41 +0200

    implemented api and textviews for data

--- app/src/main/java/si/uni_lj/fe/tnuv/deckbuilder/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/deckbuilder/MainActivity.java
@@ -7,6 +7,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,17 +18,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import si.uni_lj.fe.tnuv.deckbuilder.ui.login.loginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textJson;
+    TextView cardName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         //textJson = (TextView) findViewById(R.id.tvJsonItem);
 
-        new JsonTask().execute("https://db.ygoprodeck.com/api/v7/cardinfo.php?name");
+        new JsonTask().execute();
     }
 
     public void loginActivity(View v) {
@@ -50,73 +60,57 @@ public class MainActivity extends AppCompatActivity {
 
 class JsonTask extends AsyncTask<String, String, String> {
 
-    String data ="";
-    String singleParsed ="";
-
-   // String[] imenaKart = ;
-
+    @Override
     protected String doInBackground(String... params) {
-
-
-        HttpURLConnection connection = null;
-        BufferedReader reader = null;
-
         try {
-            URL url = new URL(params[0]);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
+            // Create a new HTTP Client
+            DefaultHttpClient defaultClient = new DefaultHttpClient();
+            // Setup the get request
+            HttpGet httpGetRequest = new HttpGet("https://db.ygoprodeck.com/api/v7/cardinfo.php");
 
+            // Execute the request in the client
+            HttpResponse httpResponse = defaultClient.execute(httpGetRequest);
+            // Grab the response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8"));
+            String json = reader.readLine(); //sparsano v json string
 
-            InputStream stream = connection.getInputStream();
+            //Log.d("res", json); //koncno dobis JSON kot raw (json) STRING
 
-            reader = new BufferedReader(new InputStreamReader(stream));
+            JSONObject jObj = new JSONObject(json);  //ustvarmo json objekt zato da iz json stringa preberemo data k je napisan json objekt
+            JSONArray jsonArry = jObj.getJSONArray("data");  //dobimo json array k je sparsan data ven
 
-            StringBuffer buffer = new StringBuffer();
-            String line = "";
+            Cards[] cardsArray = new Gson().fromJson(jsonArry.toString(), Cards[].class);  //json array damo nazaj v string
 
-            while ((line = reader.readLine()) != null) {
-                buffer.append(line+"\n");
-                Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
-                data = data + line;
-                //Log.d("Prebran data", "> ", data);
+            //System.out.println(cardsArray.length);
+            //System.out.println(cardsArray[0].name);
+
+            System.out.println(cardsArray.toString());
+
+            for (Cards card: cardsArray)
+            {
+                System.out.println("NAME :"+card.name+" DESCRIPTION: "+card.desc+"\nATTACK: "+card.atk+" DEFENSE: "+card.def);
             }
 
-            JSONArray JA = new JSONArray(data);
-            for(int i =0 ;i <JA.length(); i++){
-                JSONObject JO = (JSONObject) JA.get(i);
-                singleParsed =  "Name:" + JO.get("name") + "\n";
+            /*for (int i = 0; i < 5; i++) {
+                System.out.println("Name :"+cardsArray[i].name+" desc: "+cardsArray[i].desc);
+            }*/
 
-                //dataParsed = dataParsed + singleParsed +"\n" ;
-            }
+            // Instantiate a JSON object from the request response
+            //JSONObject jsonObject = new JSONObject(json);
+            //System.out.println(jsonObject);
 
-            return buffer.toString();
-
-
-        } catch (MalformedURLException e) {
+        } catch(Exception e){
+            // In your production code handle any errors and catch the individual exceptions
             e.printStackTrace();
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+
         return null;
     }
+}
 
-    /*@Override
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-        if (pd.isShowing()){
-            pd.dismiss();
-        }
-        textJson.setText(result);
-    }*/
+class Cards {
+    String name;
+    String desc;
+    String atk;
+    String def;
 }
--- app/src/main/java/si/uni_lj/fe/tnuv/deckbuilder/deck/DeckActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/deckbuilder/deck/DeckActivity.java
@@ -3,6 +3,9 @@ package si.uni_lj.fe.tnuv.deckbuilder.deck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import si.uni_lj.fe.tnuv.deckbuilder.R;
 
@@ -12,5 +15,19 @@ public class DeckActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deck);
+
+        //ScrollView vseKarte = findViewById(R.id.containerKart);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.karteLayout);
+        TextView textView = new TextView(this);
+        textView.setText("haha1223542342353");
+        textView.setHeight(100);
+        textView.setWidth(100);
+        ll.addView(textView);
+
+       /*for (int i = 0; i < 5; i++) {
+           TextView textView = new TextView(this);
+           textView.setText("haha"+i);
+           ll.addView(textView);
+       */
     }
 }
