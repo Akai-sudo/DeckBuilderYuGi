fdb34d25bf8c47936a9904a70e23f3bea14bd591
Akai-sudo <mohorluka@gmail.com>
2022-06-20 12:38:58 +0200

    layout changes

--- app/src/main/java/si/uni_lj/fe/tnuv/deckbuilder/HomeActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/deckbuilder/HomeActivity.java
@@ -52,6 +52,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
